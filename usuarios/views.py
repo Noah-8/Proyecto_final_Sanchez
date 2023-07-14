6234b7cdecab353aefc44a8700cb1133e0cc76e0
Noah Sanchez <nbanoah.8@gmail.com>
2023-07-14 10:56:11 -0300

    cuarto prototipo, lista la edicion del perfil y los avatares

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import authenticate, login as django_login
-from usuarios.form import MiFormularioDeCreacionDeUsuarios
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from usuarios.form import MiFormularioDeCreacionDeUsuarios, MiFormularioDeEdicionDeDatosDeUsuario
+from django.urls import reverse_lazy
+from usuarios.models import InfoExtra
 
 # Create your views here.
 
@@ -16,12 +21,16 @@ def login(request):
             user = authenticate(username=usuario, password=contrasenia)
             
             django_login(request, user)
+            
+            InfoExtra.objects.get_or_create(user=user)
+            
             return redirect('inicio:inicio')
         else:
             return render(request, 'usuarios/login.html', {'formulario': formulario})
     
     formulario = AuthenticationForm()
     return render(request, 'usuarios/login.html', {'formulario': formulario})
+
 
 def registrarse(request):
     
@@ -35,3 +44,28 @@ def registrarse(request):
     
     formulario = MiFormularioDeCreacionDeUsuarios()
     return render(request, 'usuarios/registro.html', {'formulario': formulario})
+
+@login_required
+def edicion_perfil(request):
+    info_extra_user = request.user.infoextra
+    if request.method == 'POST':
+        formulario = MiFormularioDeEdicionDeDatosDeUsuario(request.POST, request.FILES, instance=request.user)
+        if formulario.is_valid():
+            
+            avatar = formulario.cleaned_data.get('avatar')
+            if avatar:
+                info_extra_user.avatar = avatar
+                info_extra_user.save()
+            
+            formulario.save()
+            return redirect('inicio:inicio')
+        else:
+            return render(request, 'usuarios/edicion_perfil.html', {'formulario': formulario})
+    
+    formulario = MiFormularioDeEdicionDeDatosDeUsuario(initial={'avatar': info_extra_user.avatar}, instance=request.user)
+    return render(request, 'usuarios/edicion_perfil.html', {'formulario': formulario})
+
+
+class ModificarPass(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'usuarios/modificar_pass.html'
+    success_url = reverse_lazy('usuarios:editar_perfil')
